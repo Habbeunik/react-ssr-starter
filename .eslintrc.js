fe952e8d8243389312f1cdc524eb24ccdd4d1005
Kumapayi Abbey <habbeyunik@gmail.com>
2020-09-09 13:12:31 +0100

    setup prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,25 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'node': true,
-  },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
-  ],
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
-    },
-    'ecmaVersion': 12,
-    'sourceType': 'module',
-  },
-  'plugins': [
-    'react',
-  ],
-  'rules': {
-  },
+	env: {
+		browser: true,
+		es2021: true,
+		node: true
+	},
+	extends: [
+		'plugin:react/recommended',
+		'google',
+		'prettier',
+		'stylelint-config-prettier'
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 12,
+		sourceType: 'module'
+	},
+	plugins: ['react'],
+	rules: {
+		'prettier/prettier': 'warn',
+		'require-jsdoc': 0
+	}
 };
