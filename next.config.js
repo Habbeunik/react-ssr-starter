251a5837ba2194e1d16b6ed9e94660ebffb4ef96
Kumapayi Abbey <habbeyunik@gmail.com>
2020-09-09 14:26:38 +0100

    image loader setup

--- next.config.js
+++ next.config.js
@@ -1,5 +1,30 @@
-const withSass = require('@zeit/next-sass');
+const withPlugins = require('next-compose-plugins');
 
-module.exports = withSass({
-	cssModules: true
-});
+const sass = require('@zeit/next-sass');
+const images = require('next-images');
+
+module.exports = withPlugins(
+	[
+		[
+			sass,
+			{
+				cssModules: true
+			}
+		],
+		[images]
+	],
+	{
+		webpack: (config) => {
+			config.module.rules.push({
+				test: /\.(eot|ttf|woff|woff2)$/i,
+				use: [
+					{
+						loader: 'file-loader'
+					}
+				]
+			});
+
+			return config;
+		}
+	}
+);
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.scss';
+import Img from './vercel.svg';
 
 export default function Home() {
 	return (
@@ -10,6 +11,7 @@ export default function Home() {
 			</Head>
 
 			<main className={styles.main}>
+				<img src={Img} />
 				<h1 className={styles.title}>
 					Welcome to <a href='https://nextjs.org'>Next.js!</a>
 				</h1>
